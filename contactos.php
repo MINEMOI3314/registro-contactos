3311409285c8152284e6f370155348c55f478754
MINEMOI3314 <minemoi197@gmail.com>
2024-11-01 21:57:10 -0600

    implemente lo de que se puedan ver los contactos guardados

--- contactos.php
+++ contactos.php
@@ -1,3 +1,6 @@
+<?php
+    include("db.php");
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -17,59 +20,80 @@
     </header>
 
     <div class="contacts-table">
-        <div class="contact-row">
-            <div class="contact-photo">FOTO</div>
-            <div class="contact-info"></div>
-            <div class="contact-actions">
-                <button>Eliminar</button>
-                <button><a href="editar.html">Modificar</a></button>
-            </div>
-        </div>
+        <?php
+            //cuento cuantos contactos tengo agregados
+            $sql_select = "SELECT COUNT(*) FROM `contacto`";
+            $sql_result = mysqli_query($conn, $sql_select);
+            $table_count = $sql_result->fetch_row();
 
-        <div class="contact-row">
-            <div class="contact-photo">FOTO</div>
-            <div class="contact-info"></div>
-            <div class="contact-actions">
-                <button>Eliminar</button>
-                <button><a href="editar.html">Modificar</a></button>
-            </div>
-        </div>
+            //loop para mostrar todos los contactos guardados
+            for($iterador=1; $iterador<=$table_count[0]; $iterador++){
 
-        <div class="contact-row">
-            <div class="contact-photo">FOTO</div>
-            <div class="contact-info"></div>
-            <div class="contact-actions">
-                <button>Eliminar</button>
-                <button><a href="editar.html">Modificar</a></button>
-            </div>
-        </div>
+                //extraigo el nombre y la ubicacion de la foto en un array
+                $sql_select = "SELECT * FROM `contacto` WHERE $iterador = id_contacto";
+                $sql_result = mysqli_query($conn, $sql_select);
+                $sql_key_contacto = $sql_result->fetch_row();
 
-        <div class="contact-row">
-            <div class="contact-photo">FOTO</div>
-            <div class="contact-info"></div>
-            <div class="contact-actions">
-                <button>Eliminar</button>
-                <button><a href="editar.html">Modificar</a></button>
+                //extraigo los numeros y los meto en un array
+                $sql_select = "SELECT numero FROM `numero` WHERE $iterador = id_contacto";
+                $sql_result = mysqli_query($conn, $sql_select);
+                $i = 0;
+                while($sql_key_numero_fetch = $sql_result->fetch_row()){
+                    $key_numero[$i] = $sql_key_numero_fetch[0];
+                    $i++;
+                }
+                
+                //extraigo los correos y los meto a un array
+                $sql_select = "SELECT correo FROM `correo` WHERE $iterador = id_contacto";
+                $sql_result = mysqli_query($conn, $sql_select);
+                $i = 0;
+                while($sql_key_correo_fetch = $sql_result->fetch_row()){
+                    $key_correo[$i] = $sql_key_correo_fetch[0];
+                    $i++;
+                }
+                
+                echo "
+                <div class=\"contact-row\">
+                <div class=\"contact-photo\">
+                    <img id=\"foto\" src=\"$sql_key_contacto[2]\" style=\"height: 150px\">
+                </div>
+                <div class=\"contact-info\">
+                NOMBRE: $sql_key_contacto[1]<br>
+                NUMERO(S):<br>$key_numero[0]<br>";
+                if (isset($key_numero[1])){
+                    echo"$key_numero[1]<br>";
+                }
+                if (isset($key_numero[2])){
+                    echo"$key_numero[2]<br>";
+                }
+                echo "CORREO(S):<br>$key_correo[0]<br>";
+                
+                if (isset($key_correo[1])){
+                    echo"$key_correo[1]<br>";
+                }
+                if (isset($key_correo[2])){
+                    echo"$key_correo[2]<br>";
+                }
+                echo "</div>
+                <div class=\"contact-actions\">
+                    <button>Eliminar</button>
+                    <button><a href=\"editar.php\">Modificar</a></button>
+                </div>
             </div>
-        </div>
+                ";
+                unset($key_correo);
+                unset($key_numero);
+            }
+        ?>
 
-        <div class="contact-row">
-            <div class="contact-photo">FOTO</div>
-            <div class="contact-info"></div>
-            <div class="contact-actions">
-                <button>Eliminar</button>
-                <button><a href="editar.html">Modificar</a></button>
-            </div>
-        </div>
 
-        <div class="contact-row">
-            <div class="contact-photo">FOTO</div>
-            <div class="contact-info"></div>
-            <div class="contact-actions">
-                <button>Eliminar</button>
-                <button><a href="editar.html">Modificar</a></button>
-            </div>
-        </div>
+
+
+
+        
+
+        
+
     </div>
 
 </body>
